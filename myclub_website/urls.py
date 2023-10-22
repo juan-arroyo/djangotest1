6ebfc935ff97367da4fbc5051ffca29514238ffb
Juan Arroyo <juanmnarroyo@gmail.com>
2023-10-23 00:00:05 +0200

    static files

--- myclub_website/urls.py
+++ myclub_website/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Configure Admin Titles
 admin.site.site_header = "My Club Administration Page"
