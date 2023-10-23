e50a4ae4ebdeed48d03d52be4f3583e57b39b4dd
Juan Arroyo <juanmnarroyo@gmail.com>
2023-10-23 22:23:28 +0200

    test

--- myclub_website/settings.py
+++ myclub_website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+5ovad%m0!3i(n+0i20%s-l$7=tu9m&jh9+1%3dorl1fw+aq31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangojma.es']
 
 
 # Application definition
