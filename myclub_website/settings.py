fae6b0534619f9d1f3b2c98ec776afaea699b509
Juan Arroyo <juanmnarroyo@gmail.com>
2023-10-24 14:24:32 +0200

    header and settings edit

--- myclub_website/settings.py
+++ myclub_website/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+5ovad%m0!3i(n+0i20%s-l$7=tu9m&jh9+1%3dorl1fw+aq31'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['djangojma.es']
+ALLOWED_HOSTS = ['djangojma.es', '127.0.0.1']
 
 
 # Application definition
